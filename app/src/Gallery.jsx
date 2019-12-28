2be399e597567cb98b58d02366a6951e9dd49239
Timotej Gale <timotej.gale@gmail.com>
2019-12-28 23:26:39 +0100

    Implement image upload

--- app/src/Gallery.jsx
+++ app/src/Gallery.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTimes, faChevronLeft, faChevronRight, faShare, faTrash, faComments } from "@fortawesome/free-solid-svg-icons";
+import { faTimes, faChevronLeft, faChevronRight, faShare, faTrash, faComments, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Fab } from 'react-tiny-fab';
+import 'react-tiny-fab/dist/styles.css';
+import { Modal, Button, Spinner } from 'react-bootstrap';
 
 import sample1 from './static/img/sample1.jpg';
 import sample2 from './static/img/sample2.jpg';
 import sample3 from './static/img/sample3.jpg';
 
-library.add(faTimes, faChevronLeft, faChevronRight, faShare, faTrash, faComments);
+library.add(faTimes, faChevronLeft, faChevronRight, faShare, faTrash, faComments, faPlus);
 
 export default class Gallery extends React.Component {
   constructor(props) {
@@ -16,6 +19,8 @@ export default class Gallery extends React.Component {
       images: [],
       isImageShown: false,
       selectedImage: null,
+      showUploadModal: false,
+      isUploading: false,
     };
   }
 
@@ -53,10 +58,76 @@ export default class Gallery extends React.Component {
     }
   }
 
+  handleCloseUploadModal = () => {
+    if (!this.state.isUploading) {
+      this.setState({
+        showUploadModal: false,
+        uploadError: "",
+      });
+    }
+  }
+
+  handleOpenUploadModal = () => {
+    this.setState({
+      showUploadModal: true,
+    });
+  }
+
+
+
+  uploadImage = () => {
+    let imageInput = document.getElementById('imageFile');
+    if (!imageInput.files || imageInput.files.length <= 0) {
+      this.setState({ uploadError: "Please select a file." });
+      return;
+    }
+    if (imageInput.files.length > 1) {
+      this.setState({ uploadError: "Please select one file." });
+      return;
+    }
+    if (!imageInput.files[0].type.startsWith("image/")) {
+      this.setState({ uploadError: "The selected file must be an image." });
+      return;
+    }
+
+    this.setState({ isUploading: true, uploadError: "" });
+
+    // TODO: API call.
+    setTimeout(() => {
+      // TODO: Get the new list of images.
+      this.setState({ isUploading: false, showUploadModal: false });
+    }, 1000);
+  }
+
   render() {
-    const { images, isImageShown, selectedImage } = this.state;
+    const { images, isImageShown, selectedImage, showUploadModal, uploadError, isUploading } = this.state;
+    const { shared } = this.props;
     return (
         <React.Fragment>
+          {!isImageShown && !shared && !showUploadModal && <Fab
+            icon={<FontAwesomeIcon icon="plus" />}
+            onClick={this.handleOpenUploadModal}
+          />}
+
+          <Modal show={showUploadModal} onHide={this.handleCloseUploadModal}>
+            <Modal.Header>
+              <Modal.Title>Upload an image</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>Please select an image.</p>
+              <input disabled={isUploading} type="file" id="imageFile" accept="image/*" />
+              <p style={{ color: "red" }}>{uploadError}</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" disabled={isUploading} onClick={this.handleCloseUploadModal}>
+                Close
+              </Button>
+              <Button variant="primary" disabled={isUploading} onClick={this.uploadImage}>
+                {isUploading? <span><Spinner size="sm" animation="grow" />Uploading...</span> : <span>Upload</span>}
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
           <div style={{ padding: "2%" }}>
             {images && images.length > 0
               ? images.map((img, i) => {
